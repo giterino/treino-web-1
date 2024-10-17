3ce6a0142830a08ba9007658c21d1cf217022791
Marco <nanoczk@protonmail.com>
2024-10-17 12:19:42 -0300

    cria modelo para docente
    adicionei app relatorios em projeto_esi/settings.py
    criei um modelo base para Docente
    criei e apliquei as migracoes com os comandos:
    python3 manage.py makemigrations
    python3 manage.py migrate
    adicionei via shell um docente

--- relatorios/models.py
+++ relatorios/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Docente(models.Model):
+    nome = models.CharField(max_length=64)
+    nusp = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.nusp}, {self.nome}"
