17070ed9ce808488d6003ae1a43c2a0d40b85186
Marco <nanoczk@protonmail.com>
2024-10-17 16:31:17 -0300

    cria modelo para aluno

--- relatorios/models.py
+++ relatorios/models.py
@@ -7,4 +7,18 @@ class Docente(models.Model):
     nusp = models.IntegerField()
 
     def __str__(self):
-        return f"{self.id}: {self.nusp}, {self.nome}"
+        return f"Docente {self.id}: {self.nusp}, {self.nome}"
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=64)
+    nusp = models.IntegerField()
+    orientador = models.ForeignKey(
+        Docente,
+        models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="orientandos",
+    )
+
+    def __str__(self):
+        return f"Aluno {self.id}: {self.nusp}, {self.nome}"
